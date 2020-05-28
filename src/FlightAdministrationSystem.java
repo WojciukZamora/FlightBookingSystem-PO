f8bf8a5dce002947af53518f2bed0d4e2445c5b4
ondreo <65464616+ondreo@users.noreply.github.com>
2020-05-29 01:00:17 +0200

    Dodanie klasy Airport oraz pliku z listą lotnisk

--- src/FlightAdministrationSystem.java
+++ src/FlightAdministrationSystem.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
- * @author ondreo
+ * @author Andrzej Zamora
  */
 public class FlightAdministrationSystem {
+    LinkedList<Airport> = new LinkedList <~>();
+    public void importAirports(){
+        File file = new File("Files/Airports.txt");
+        Scanner scanner = new Scanner(file);
+    }
+
 
 }
