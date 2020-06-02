a731bf717cbf902b19fcf68d2d6dcd50e87a7c23
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2020-06-02 20:51:35 +0200

    Tworzenie menu w main
    Co-Authored-By: ondreo <ondreo@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,32 @@
  * @author Paweł Wojciuk
  */
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         FlightAdministrationSystem admin = new FlightAdministrationSystem();
         admin.importAirports();
+        System.out.println("Witaj w systemie zarządzania lotami.");
+        System.out.println("1. Zarządzaj lotniskami");
+        System.out.println("2. Zarządzaj kientami");
+        System.out.println("3. Zarządzaj samolotami");
+        System.out.println("4. Zarządazaj lotami");
+        System.out.println("5. Zakończ działanie programu");
+
+        System.out.println("HAHAHAH");
+
+        int x;
+        switch (x){
+            case 1:
+                System.out.println("Sprawawdź dostępne lotniska:");
+                break;
+            case 2:
+                sout
+        }
+
+
 
 
         admin.exportAirports();
